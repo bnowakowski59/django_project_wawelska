f49e1d2c8c7f9a2c325978afeb5e9c6209b07d7b
bnowakowski59 <bnowakowski59@gmail.com>
2021-02-02 18:34:24 +0100

    add

--- cars/urls.py
+++ cars/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from cars.views import showAllCars, newCar, editCar, deleteCar
 
 urlpatterns = [
-    path('all/', showAllCars, name='showAllCars'),
+    path('', showAllCars, name='showAllCars'),
     path('new/', newCar, name='newCar'),
     path('edit/<int:id>', editCar, name='editCar'),
     path('delete/<int:id>', deleteCar, name='deleteCAr'),
--- wawelska/urls.py
+++ wawelska/urls.py
@@ -21,8 +21,8 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cars/', include('cars.urls')),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('', include('cars.urls')),
+    path('login', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
